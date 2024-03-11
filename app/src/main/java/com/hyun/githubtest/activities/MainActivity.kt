155b778cf1fd7893e6e7cf9a2fbbdcbc549755c4
chlguss97 <chlguss97@gmail.com>
2024-03-11 13:40:43 +0900

    add SecondActivity & image file

--- app/src/main/java/com/hyun/githubtest/activities/MainActivity.kt
+++ app/src/main/java/com/hyun/githubtest/activities/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.hyun.githubtest
+package com.hyun.githubtest.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hyun.githubtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/hyun/githubtest/activities/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.hyun.githubtest.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyun.githubtest.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
